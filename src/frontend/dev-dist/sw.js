3c90ef68d0f992644993e44dceb9fd506e91179a
Kevin Adrian Gil Soto <kevings0712@MacBook-Pro-de-Kevin-2.local>
2025-12-17 23:28:44 -0500

    frontend: PWA, notificaciones y ajustes finales de UI

--- src/frontend/dev-dist/sw.js
+++ src/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6d1fb45c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0at9j5o3hro"
+    "revision": "0.krnihrjnca"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
